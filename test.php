64d56155c3c432d640a8d4a9ce3c93448d605a9f
Joel <49372023+Joel-Sousa@users.noreply.github.com>
2020-05-06 19:02:52 -0300

    Update test.php

--- test.php
+++ test.php
@@ -1,11 +1,4 @@
  
-<?php
-
-try{
-        new PDO('mysql:host=localhost;dbname=teste','root','23r0');
+<?php try{ new PDO('mysql:host=localhost;dbname=teste','root','23r0');
         echo "Conexão efetuada com sucesso!";
-}catch(PDOException $ex){
-        echo $ex->getMessage();
-}
-//echo "teste"
-?>
+}catch(PDOException $ex){ echo $ex->getMessage();}?>
