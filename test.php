901bd7ff9abd63e2f91179d285547491a70ee86c
Joel Sousa <softmock@gmail.com>
2021-01-22 10:01:13 -0300

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
  
-<?php try{ new PDO('mysql:host=localhost;dbname=teste','root','23r0');
+<?php try{ new PDO('mysql:host=localhost;dbname=teste','usuario','senha');
         echo "Conexão efetuada com sucesso!";
 }catch(PDOException $ex){ echo $ex->getMessage();}?>
 
